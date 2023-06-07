bd756a5dc8635205d3f39a7112a10e51a4ac5998
Tetiana Semenchuk <tsamolyuk@gmail.com>
2023-06-08 02:22:31 +0300

    add new site

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./img/logo.svg";
+import photo_1 from "./img/miguelangel-miquelena.png";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="header">
+        <img src={logo} className="logo" alt="logo" />
       </header>
+      <main className="main">
+        <div className="container">
+          <h6>Testimonials</h6>
+          <h1>What our clients say about us</h1>
+          <section className="section1">
+            <div className="textBox">
+              <p className="textBox-p1">
+                I have worked with a number of software developers before but
+                none of them quite reached the level of professionalism,
+                diligence and enthusiasm that I experienced with Vitaliy and his
+                team. They are incredibly easy to work with and almost always
+                find great programming solutions to our requirements.
+                <br />
+                <br />
+                <br />
+                What I especially liked about the SoftFormance team is their
+                passion and understanding for our project. It’s like working
+                with friends. Friends that deliver on their promises. I have
+                nothing but praise for the work of SoftFormance team and look
+                forward to working with this agency in the future.
+              </p>
+              <p className="textBox-p2">
+                <b>Wolfgang Männel</b>
+                <br />
+                Founder and Managing Director, Fotografen365
+              </p>
+            </div>
+
+            <div className="photoBox">
+              <div className="photoBox-inner">
+                <img src={photo_1} alt="photo-1" height="100%"></img>
+                <a className="btn-2 btn2-left">&#10093;</a>
+                <a className="btn-2 btn2-right">&#10092;</a>
+              </div>
+            </div>
+          </section>
+          <section className="section2">
+            <h1>Leave your feedback</h1>
+            <p>
+              If you had a chance to work with us, please, leave your feedback{" "}
+              <br /> and we’ll happily add it to our testimonials page. Thank
+              you!
+            </p>
+
+            <a className="btn-1">leave your feedback</a>
+          </section>
+        </div>
+      </main>
     </div>
   );
 }
